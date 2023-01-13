3dda53248a5ce9292df6b08945b36d1c9fc3478c
Marco Born <marco.born345@gmail.com>
2023-01-13 21:07:25 +0100

    Refactor of UI in search option screen

--- frontend/lib/search/screens/search_option_screen.dart
+++ frontend/lib/search/screens/search_option_screen.dart
@@ -1,3 +1,4 @@
+import 'package:flutter/cupertino.dart';
 import 'package:flutter/material.dart';
 import 'package:frontend/search/screens/search_dish_name.dart';
 import 'package:frontend/search/widgets/search_option_container.dart';
@@ -14,37 +15,58 @@ class SearchOptionScreen extends StatelessWidget {
   @override
   Widget build(BuildContext context) {
     return Scaffold(
+      appBar: AppBar(
+          leading: const Icon(CupertinoIcons.search,
+              color: Colors.white),
+          title: Text("Search using..."),
+        ),
       backgroundColor: const Color(0xFF242424),
-      body: Column(
-          mainAxisAlignment: MainAxisAlignment.start,
-          crossAxisAlignment: CrossAxisAlignment.center,
-          children: [
-            const Padding(
-              padding: EdgeInsets.only(left: 10, right: 10, top: 75, bottom: 60),
-              child: Text(
-                "Search using:",
-                style: TextStyle(
-                  color: Colors.white, 
-                  fontSize: 32,
+      body: SafeArea(
+        child: Column(
+            mainAxisAlignment: MainAxisAlignment.center,
+            crossAxisAlignment: CrossAxisAlignment.center,
+            children: [
+              // const Padding(
+              //   padding:  EdgeInsets.only(top: 20.0),
+              //   child: Icon(
+              //     CupertinoIcons.search,
+              //     size: 100,
+              //     color: Colors.white,
+              //   ),
+              // ),
+              const Padding(
+                padding: EdgeInsets.only(left: 10, right: 10, top: 00, bottom: 40),
+                child: Text(
+                  "Search recipes using...",
+                  style: TextStyle(
+                    color: Colors.white, 
+                    fontSize: 32,
+                  ),
                 ),
               ),
-            ),
-            GestureDetector(
-              onTap: () => {
-                Navigator.push(
-                  context,
-                  MaterialPageRoute(
-                    builder: (context) => const SearchDishName()
+              GestureDetector(
+                onTap: () => {
+                  Navigator.push(
+                    context,
+                    MaterialPageRoute(
+                      builder: (context) => const SearchDishName()
+                    )
                   )
+                },
+                child: Padding(
+                  padding: const EdgeInsets.symmetric(horizontal: 10.0),
+                  child: SearchOptionContainer(titleList[0], descList[0]),
                 )
-              },
-              child: SearchOptionContainer(titleList[0], descList[0])
-            ),
-            GestureDetector(
-              onTap: () => {},
-              child: SearchOptionContainer(titleList[1], descList[1])
-            ),
-          ]),
+              ),
+              GestureDetector(
+                onTap: () => {},
+                child: Padding(
+                  padding: const EdgeInsets.symmetric(horizontal: 10.0),
+                  child: SearchOptionContainer(titleList[1], descList[1]),
+                )
+              ),
+            ]),
+      ),
     );
   }
 }
