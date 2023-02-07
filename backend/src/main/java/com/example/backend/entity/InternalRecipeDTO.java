e8c3f72563d845bcd52430ca66c62d508be15223
Marco Born <marco.born345@gmail.com>
2023-02-07 16:59:27 +0100

    Added constructor to allow converting from Recipe

--- backend/src/main/java/com/example/backend/entity/InternalRecipeDTO.java
+++ backend/src/main/java/com/example/backend/entity/InternalRecipeDTO.java
@@ -1,23 +1,58 @@
 package com.example.backend.entity;
 
 import lombok.Getter;
+import lombok.NoArgsConstructor;
 import lombok.Setter;
 
 import java.util.ArrayList;
+import java.util.HashMap;
 import java.util.Map;
 
-// Used for getting info from Firebase database
+// Used for getting info from/to Firebase database
 @Getter
 @Setter
+@NoArgsConstructor
 public class InternalRecipeDTO {
+    public InternalRecipeDTO(Recipe recipe) {
+        id = recipe.getId();
+        title = recipe.getTitle();
+        calories = recipe.getCalories();
+        proteins = recipe.getProteins();
+        carbohydrates = recipe.getCarbohydrates();
+        fats = recipe.getFats();
+        readyInMinutes = recipe.getReadyInMinutes();
+        imageURL = recipe.getImageURL();
+        servings = recipe.getServings();
+        author = recipe.getAuthor();
+        dateAdded = recipe.getDateAdded();
+        steps = recipe.getSteps();
+        ingredients = new ArrayList<>();
+
+        ArrayList<Ingredient> ingredientArrayList = recipe.getIngredients();
+
+        Map<String, Map<String, Object>> converted_ingredient_map = new HashMap<>();
+        for (Ingredient ingredient: ingredientArrayList) {
+            ingredients.add(ingredient.getName());
+
+            Map<String, Object> ingredientInfo = new HashMap<>();
+            ingredientInfo.put("id", ingredient.getId());
+            ingredientInfo.put("amount", ingredient.getAmount());
+            ingredientInfo.put("unit", ingredient.getUnit());
+
+            converted_ingredient_map.put(ingredient.getName(), ingredientInfo);
+        }
+
+        ingredients_map = converted_ingredient_map;
+    }
+
     private int id;
     private String title;
     private ArrayList<String> ingredients;
     private Map<String, Map<String, Object>> ingredients_map;
-    private Object calories;
-    private Object proteins;
-    private Object carbohydrates;
-    private Object fats;
+    private Double calories;
+    private Double proteins;
+    private Double carbohydrates;
+    private Double fats;
     private ArrayList<String> steps;
 
     private double readyInMinutes;
